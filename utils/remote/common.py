e743cd5cd313ff587250b0a44ac7dd1ad4df16a3
Leopold Schabel <leopold.schabel@continum.net>
2014-06-12 17:52:25 +0200

    Importing existing code (PWSafe utils)

---
+++ utils/remote/common.py
@@ -0,0 +1,181 @@
+from contextlib import contextmanager
+import socket
+import paramiko
+from subprocess import list2cmdline
+
+from utils import pexpect
+
+__author__ = 'lschabel'
+
+
+def sanitize(value):
+    return list2cmdline([value])
+
+
+class RemoteException(RuntimeError):
+    pass
+
+
+class ConnectionError(RemoteException):
+    pass
+
+
+class OperationalError(RemoteException):
+    pass
+
+
+class LoginFailedException(RemoteException):
+    pass
+
+
+class BaseRemoteControl(object):
+    def __init__(self, hostname):
+        self.hostname = hostname
+
+
+# TODO: verbose logging
+
+class SSHRemoteControl(BaseRemoteControl):
+    def __init__(self, hostname, port=22, hostkey=None, timeout=3, cmd_timeout=10):
+        super(SSHRemoteControl, self).__init__(hostname)
+
+        self.transport = None
+        self.port = port
+        self.hostkey = hostkey
+        self.chan = None
+        self.username = None
+        self.password = None
+        self.timeout = timeout
+        self.interact = None
+        self.cmd_timeout = cmd_timeout
+        self.allocate_pty = True
+
+    def _connect_transport(self, transport, username=None, password=None):
+
+        if not username:
+            username = self.username
+        if not password:
+            password = self.password
+
+        try:
+            transport.start_client()
+        except paramiko.SSHException:
+            raise ConnectionError("SSH negotiation failed")
+
+        # TODO: verify host key here
+
+        try:
+            transport.auth_password(username, password)
+
+            # double check, auth_password should already raise the exception
+            if not transport.is_authenticated():
+                raise paramiko.AuthenticationException()
+
+        except paramiko.AuthenticationException:
+            raise LoginFailedException("SSH login failed for user %s" % username)
+
+    def _make_transport(self):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            if self.timeout:
+                sock.settimeout(self.timeout)
+            sock.connect((self.hostname, self.port))
+            return paramiko.Transport(sock)
+        except (paramiko.SSHException, socket.timeout), e:
+            raise ConnectionError("SSH socket failed: %s" % e.message)
+
+    def connect(self, username, password):
+        self.username = username
+        self.password = password
+
+        self.transport = self._make_transport()
+        self._connect_transport(self.transport)
+        self.chan = self.transport.open_session()
+        self.chan.settimeout(self.timeout)
+        if self.allocate_pty:
+            self.chan.get_pty()
+        self.chan.invoke_shell()
+        self.interact = pexpect.SSHClientInteraction(self.chan, timeout=self.cmd_timeout)
+
+    def close(self):
+        if self.chan:
+            self.chan.close()
+        if self.transport:
+            self.transport.close()
+
+    def verify_login(self, username, password):
+        transport = None
+
+        try:
+            transport = self._make_transport()
+            self._connect_transport(transport, username, password)
+        except LoginFailedException:
+            return False
+        else:
+            return transport.is_authenticated()
+        finally:
+            if transport:
+                transport.close()
+
+
+class FirewallRemoteControl(SSHRemoteControl):
+
+    def __init__(self, hostname, port=22, hostkey=None, timeout=5, cmd_timeout=10):
+        super(FirewallRemoteControl, self).__init__(hostname, port, hostkey, timeout, cmd_timeout)
+        self.allocate_pty = False
+
+    def change_admin_password(self, new_password, verify=True):
+        raise NotImplementedError
+
+    def add_admin(self, username, password, privilege="read-only",
+                  role=None, sshkey=None, ssh_only_key=False, verify=True):
+        raise NotImplementedError
+
+    def enable_scp(self):
+        raise NotImplementedError
+
+    def save_and_apply(self):
+        raise NotImplementedError
+
+    def expect_prompt(self):
+        raise NotImplementedError
+
+    def read_config(self):
+        raise NotImplementedError
+
+    def setup_terminal(self):
+        raise NotImplementedError
+
+    def teardown_terminal(self):
+        raise NotImplementedError
+
+    def run_command(self, command):
+        self.interact.send(command)
+        self.expect_prompt()
+        return self.interact.current_output_clean
+
+    @contextmanager
+    def ctx_term_setup(self):
+        try:
+            self.setup_terminal()
+            yield
+        finally:
+            self.teardown_terminal()
+
+
+class GuessingFirewallRemoteControl(FirewallRemoteControl):
+    def guess_type(self):
+        prompt = self.chan.recv(1024)
+
+        if "Remote Management Console" in prompt:
+            return "Juniper"
+        elif "#" in prompt:
+            # if it's not a Juniper, simply assume it's a Fortigate firewall
+            return "Fortigate"
+
+
+if __name__ == '__main__':
+    import getpass
+    rc = GuessingFirewallRemoteControl("192.168.50.50")
+    rc.connect("root", getpass.getpass())
+    print rc.guess_type()
---
+++ utils/remote/fortigate.py
@@ -0,0 +1,121 @@
+from contextlib import contextmanager
+import re
+
+import common
+
+__author__ = 'lschabel'
+
+RE_SYSINFO = re.compile(r""".*^.*?Version:\s+(?P<model>.+?)\s(?P<swrev>.+?)$.*
+^Serial-Number:\s+(?P<serial>FG.+?)$""", re.DOTALL | re.MULTILINE)
+
+
+class FortigateRemoteControl(common.FirewallRemoteControl):
+    def __init__(self, *args, **kwargs):
+        super(FortigateRemoteControl, self).__init__(*args, **kwargs)
+
+    def expect_prompt(self, in_block=False):
+        if in_block:
+            self.interact.expect(r'.+\(.+\)\s*[#$].*')
+        else:
+            self.interact.expect(r'.+\s+[#$].*')
+
+    def run_command(self, command, in_block=False):
+        self.interact.send(command)
+        self.expect_prompt(in_block=in_block)
+
+        if "Command fail" in self.interact.current_output_clean:
+            raise common.OperationalError("Command '%s' failed: %s" % (
+                # TODO: strip prompt
+                command, self.interact.current_output_clean
+            ))
+        else:
+            return self.interact.current_output_clean
+
+    def change_admin_password(self, new_password, username=None, verify=True):
+        if not username:
+            username = self.username
+
+        with self.ctx_config_block("system admin"):
+            with self.ctx_edit_block(common.sanitize(username)):
+                self.run_command("set password %s" % common.sanitize(new_password), True)
+
+        if verify:
+            if not self.verify_login(username, new_password):
+                raise common.OperationalError("Password change failed")
+            else:
+                return True
+
+    def add_admin(self, username, password, privilege="prof_admin", role=None, sshkey=None, ssh_only_key=False,
+                  verify=True):
+
+        # TODO: remove redundant code
+        # TODO: abstract privileges
+
+        if role:
+            raise ValueError("Roles not supported")
+
+        if ssh_only_key:
+            raise NotImplementedError("ssh_only_key not implemented")
+
+        with self.ctx_config_block("system admin"):
+            with self.ctx_edit_block(common.sanitize(username)):
+                self.run_command("set password %s" % common.sanitize(password), True)
+                self.run_command("set accprofile %s" % common.sanitize(privilege), True)
+                self.run_command("set trusthost1 192.168.50.50 192.168.50.50", True)
+
+            if sshkey:
+                    self.run_command('set ssh-public-key1 "ssh-dss %s"' % sshkey, True)
+
+        if verify:
+            if not self.verify_login(username, password):
+                raise common.OperationalError("Failed to add new admin")
+            else:
+                return True
+
+    def enable_scp(self):
+        with self.ctx_config_block("system global"):
+            self.run_command("set admin-scp enable", True)
+
+    def read_sysinfo(self):
+        #with self.ctx_term_setup():
+        if True:
+            output = self.run_command("get system status")
+            match = RE_SYSINFO.match(output)
+
+            if match:
+                return match.groupdict()
+
+    @contextmanager
+    def ctx_config_block(self, path):
+        try:
+            self.run_command("config " + path, in_block=True)
+            yield
+        finally:
+            self.run_command("end")
+
+    @contextmanager
+    def ctx_edit_block(self, path):
+        try:
+            self.run_command("edit " + path, in_block=True)
+            yield
+        finally:
+            self.run_command("next", in_block=True)
+
+    def setup_terminal(self):
+        #FIXME: does not work on $ prompt FG (vdoms?)
+        with self.ctx_config_block('system console'):
+            self.run_command("set output standard", True)
+
+    def teardown_terminal(self):
+        with self.ctx_config_block('system console'):
+            self.run_command("set output more", True)
+
+    def close(self):
+        self.interact.send("exit")
+        super(FortigateRemoteControl, self).close()
+
+
+if __name__ == '__main__':
+    import getpass
+    rc = FortigateRemoteControl("c93f.continum.net", port=2222, timeout=1)
+    rc.connect("root", getpass.getpass())
