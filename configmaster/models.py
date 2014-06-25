4181169af6bfe761d05b5f11ed708f845f80cade
Leopold Schabel <leopold.schabel@continum.net>
2014-06-25 21:37:47 +0200

    Add enabled field to tasks

--- configmaster/models.py
+++ configmaster/models.py
@@ -45,6 +45,7 @@ class Task(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, default="")
     class_name = models.CharField(max_length=100)
+    enabled = models.BooleanField(default=True)
 
     def __unicode__(self):
         return self.name
