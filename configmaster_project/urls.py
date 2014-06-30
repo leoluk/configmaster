84c3560e3996626c67722d93158b622ca400d93e
Leopold Schabel <leopold.schabel@continum.net>
2014-06-30 18:24:37 +0200

    Base template

--- configmaster_project/urls.py
+++ configmaster_project/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+import configmaster.urls
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +11,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include(configmaster.urls))
 )
