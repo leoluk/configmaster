0574d29b67a9ea8ba7e143abb53645cb0791ad4f
Leopold Schabel <leopold.schabel@continum.net>
2014-08-12 14:43:55 +0200

    Move NetworkDeviceConfigBackupHandler to separate file
    Ref T120

--- configmaster/management/handlers/config_backup.py
+++ configmaster/management/handlers/config_backup.py
@@ -1,208 +1,18 @@
 import codecs
-from contextlib import contextmanager
 from django.conf import settings
 import os
-import paramiko
 from pipes import quote
-import re
-from sh import git
-from paramiko import SSHException
 from scp import SCPException
 import shutil
-import socket
 import tempfile
+from sh import git
 
-from configmaster.management.handlers import BaseHandler
-from configmaster.models import Credential, DeviceType
-from utils.remote.brocade import BrocadeRemoteControl
-from utils.remote.common import GuessingFirewallRemoteControl, RemoteException, \
-    NetworkDeviceRemoteControl
-from utils.remote.fortigate import FortigateRemoteControl
-from utils.remote.juniper import JuniperRemoteControl
-from utils.remote.procurve import ProCurveRemoteControl
+from configmaster.management.handlers.network_device import \
+    NetworkDeviceHandler, RE_MATCH_FIRST_WORD
+from configmaster.models import DeviceType
 
 
-RE_MATCH_FIRST_WORD = re.compile(r'\b\w+\b')
-
-
-class SSHDeviceHandler(BaseHandler):
-    def __init__(self, device):
-        """
-        A base class for SSH device handlers. Provides self.credential and
-        handles the most common OS and Paramiko connection errors. Does not
-        provide any connection facilities.
-
-        :type device: configmaster.models.Device
-        """
-
-        super(SSHDeviceHandler, self).__init__(device)
-
-        if self.device.credential:
-            self.credential = self.device.credential
-        else:
-            self.credential = self.device.device_type.credential
-        if not self.device.device_type.connection_setting:
-            self._fail("No connection setting for device")
-        elif not (self.device.device_type.connection_setting.ssh_port or
-                      self.device.device_type.connection_setting.use_ssh_config):
-            self._fail("Invalid SSH port setting")
-
-        if not self.credential or self.credential.type != Credential.TYPE_PLAINTEXT:
-            self._fail("No valid credential for device")
-
-    @contextmanager
-    def _catch_connection_errors(self):
-        try:
-            yield
-
-        # Handle network/connections errors (all other errors will be caught
-        # by the task runner and reported with full traceback).
-
-        except socket.timeout, e:
-            self._fail("Client error: Socket timeout")
-        except socket.error, e:
-            if e.strerror is None:
-                # In some cases, a socket error without any message is raised.
-                # This would result in an useless, empty error message, so
-                # we'll re-raise the exception instead to get a nice traceback.
-                raise e
-            self._fail("Socket error: %s" % e.strerror)
-        except (RemoteException, SSHException), e:
-            self._fail("Client error: %s" % str(e))
-
-    @contextmanager
-    def run_wrapper(self, *args, **kwargs):
-        with super(SSHDeviceHandler, self).run_wrapper(*args, **kwargs):
-            with self._catch_connection_errors():
-                yield
-
-
-class NetworkDeviceHandler(SSHDeviceHandler):
-    RC_CLASSES = {
-        u"Fortigate": FortigateRemoteControl,
-        u"Juniper SSG": JuniperRemoteControl,
-        u"HP ProCurve": ProCurveRemoteControl,
-        u"Brocade *Iron": BrocadeRemoteControl,
-    }
-
-    def __init__(self, device):
-        """
-        A base class for SSH-based network device handlers. Chooses the right
-        remote control class, instantiates it as self.connection and
-        connects and authenticates with the device using the credential
-        from the database.
-        """
-        super(NetworkDeviceHandler, self).__init__(device)
-
-        self.connection = self._get_fw_remote_control()
-        """:type : NetworkDeviceRemoteControl"""
-
-    @property
-    def _remote_control_class(self):
-        """
-        Returns the correct remote control class. The device type <->
-        class mapping is defined in the FW_RC_CLASSES dictionary.
-        """
-        try:
-            return self.RC_CLASSES[unicode(self.device.device_type.name)]
-        except KeyError:
-            self._fail("No device controller for device type %s",
-                       self.device.device_type)
-
-
-    # noinspection PyTypeChecker
-    def _get_fw_remote_control(self, *args, **kwargs):
-        """
-        Instantiates and returns the correct remote controller for the        # noinspection PyTypeChecker
-        device's device_type.
-
-        :rtype : NetworkDeviceRemoteControl
-        """
-
-        if self.device.device_type.connection_setting.use_ssh_config:
-            parser = paramiko.SSHConfig()
-            parser.parse(open(settings.TASK_CONFIG_BACKUP_SSH_CONFIG))
-            config = parser.lookup(self.device.hostname)
-
-            if 'port' in config:
-                ssh_port = int(config['port'])
-            else:
-                ssh_port = 22
-
-            if 'hostname' in config:
-                ssh_hostname = config['hostname']
-            else:
-                ssh_hostname = self.device.hostname
-
-        else:
-            ssh_port = self.device.device_type.connection_setting.ssh_port
-            ssh_hostname = self.device.hostname
-
-        return self._remote_control_class(
-            ssh_hostname,
-            ssh_port,
-            *args, **kwargs)
-
-    def _connect_ssh(self):
-        """
-        Does the SSH login and connection setup. This method can be
-        overwritten by descendant classes to customize the login behaviour.
-        """
-        self.connection.connect(self.credential.username,
-                                self.credential.password)
-
-    @contextmanager
-    def run_wrapper(self, *args, **kwargs):
-        with super(NetworkDeviceHandler, self).run_wrapper(*args, **kwargs):
-            self._connect_ssh()
-            yield
-            self.connection.close()
-
-
-class SSHLoginTestHandler(NetworkDeviceHandler):
-    def _connect_ssh(self):
-        # ProCurve switches close the channel if a shell without TTY is
-        # requested.
-        self.connection.allocate_pty = True
-        super(SSHLoginTestHandler, self)._connect_ssh()
-
-    @property
-    def _remote_control_class(self):
-        return NetworkDeviceRemoteControl
-
-    def run(self):
-        return self._return_success("Login successful")
-
-
-class GuessFirewallTypeHandler(NetworkDeviceHandler):
-    def __init__(self, device):
-        """
-        This handler guesses the Firewall type. Right now, it detects
-        Juniper SSG firewalls and simply assumes that everything else is a
-        Fortigate firewall. The correct device type is assigned to the
-        device if it was successfully guessed.
-
-        This task is quite useful if you assign it to a catch-all device type
-        for imported firewalls.
-        """
-
-        super(GuessFirewallTypeHandler, self).__init__(device)
-
-    @property
-    def _remote_control_class(self):
-        # Bypass the automatic mapping
-        return GuessingFirewallRemoteControl
-
-    def run(self):
-        guess = self.connection.guess_type()
-
-        if not guess:
-            self._fail("Could not guess Firewall type")
-        else:
-            device_type = DeviceType.objects.get(name=guess)
-            self.device.device_type = device_type
-            self.device.save()
-            return self._return_success("Guess: %s", guess)
+__author__ = 'lschabel'
 
 
 class NetworkDeviceConfigBackupHandler(NetworkDeviceHandler):
@@ -412,4 +222,3 @@ class NetworkDeviceConfigBackupHandler(NetworkDeviceHandler):
                 'Config filter for device type "%s" changed' % device_type.name)
 
         git.push()
-
