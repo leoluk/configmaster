f073eb7eb7c0586978e9ad0fde8d1a8cba3f7566
Leopold Schabel <leopold.schabel@continum.net>
2015-03-16 09:47:08 +0100

    Restrict sessions to IP address
    This is necessary for additional login IP address restrictions

--- configmaster_project/settings.py
+++ configmaster_project/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'restrictedsessions.middleware.RestrictedSessionsMiddleware'
 )
 
 # LDAP auth
