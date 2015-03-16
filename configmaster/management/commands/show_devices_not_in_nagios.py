290f91d2a0fbf627a7af0b6b8176c5f517955855
Leopold Schabel <leopold.schabel@continum.net>
2015-03-16 13:26:09 +0100

    Better output for show_devices_not_in_nagios
    Ref T137

--- configmaster/management/commands/show_devices_not_in_nagios.py
+++ configmaster/management/commands/show_devices_not_in_nagios.py
@@ -4,6 +4,6 @@ from configmaster.models import Device
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for device in Device.objects.all():
+        for device in Device.objects.order_by('group__name'):
             if device.latest_reports.count() and not device.known_by_nagios:
-                self.stdout.write("Device %s (%s) has reports, but is not known to Nagios" %(device.label, device.hostname))
+                self.stdout.write("%s %s (%s) has reports, but is not known to Nagios" %(device.group, device.label, device.hostname))
