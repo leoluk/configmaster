de29681e67ffcf6b064a18b3a7a00c6504adfc8b
Leopold Schabel <leopold.schabel@continum.net>
2016-03-11 12:41:30 +0100

    NTP check: improve report formatting
    Ref T240

--- configmaster/management/handlers/ntp.py
+++ configmaster/management/handlers/ntp.py
@@ -16,15 +16,16 @@ class NetworkDeviceNTPHandler(NetworkDeviceHandler):
             self.connection.get_time(),
             timezone.get_current_timezone())
 
-        delta = timezone.localtime(timezone.now()) - time
+        localtime = timezone.localtime(timezone.now())
+        delta = localtime - time
 
         if abs(delta.total_seconds()) > settings.CONFIGMASTER_NTP_MAX_DELTA:
-            raise TaskExecutionError(
-                "Time delta exceeds limit (%ds): %.02fs, date: %s" % (
+            self._fail_long_message(
+                "Time delta exceeds limit (%ds): %.02fs" % (
                     settings.CONFIGMASTER_NTP_MAX_DELTA,
                     delta.total_seconds(),
-                    time
-                )
+                ),
+                "Local time: %s\nRemote time: %s" % (localtime, time)
             )
 
         return self._return_success("Time delta: %.02f s" % delta.total_seconds())
